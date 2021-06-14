7573a3206e19fa7ba6317c58fb4fd527d158fa8e
Rust <rust.91@icloud.com>
2021-06-15 00:00:24 +0200

    Hook in services

--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -1,8 +1,10 @@
 <div class="bg-blue-200 max-w-sm mx-auto m-6 p-5 rounded-lg">
     <form>
         <label for="service" class="inline-block text-black font-semibold mb-2">Select Service</label>
-        <select name="service" id="service" class="bg-white h-10 w-full border-none rounded-lg">
-            <option value="">Cryptography</option>
+        <select name="service" id="service" class="bg-white h-10 w-full border-none rounded-lg" wire:model="state.service">
+            @foreach ($services as $service)
+                <option value="{{ $service->id }}">{{ $service->name }}</option>
+            @endforeach
         </select>
     </form>
 </div>
