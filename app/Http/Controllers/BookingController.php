15653099976d35d2f7b24547d7b30adb87fa3522
Rust <rust.91@icloud.com>
2021-06-14 23:13:29 +0200

    Fetch employee availabilities

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\Filters\AppointmentFilter;
 use App\Bookings\Filters\UnavailabilityFilter;
 use App\Bookings\TimeSlotGenerator;
+use App\Models\Appointment;
+use App\Models\Employee;
 use App\Models\Schedule;
 use App\Models\Service;
 use Carbon\CarbonInterval;
@@ -15,14 +18,10 @@ class BookingController extends Controller
     public function __invoke()
     {
         $schedule = Schedule::find(2);
-        $service = Service::find(2);
+        $service = Service::find(1);
+        $employee = Employee::find(1);
 
-        $slots = (new TimeSlotGenerator($schedule, $service))
-            ->applyFilters([
-                new SlotsPassedTodayFilter(),
-                new UnavailabilityFilter($schedule->unavailabilities)
-            ])
-            ->get();
+        $slots = $employee->availableTimeSlots($schedule, $service);
 
         return view('bookings.create', [
             'slots' => $slots
