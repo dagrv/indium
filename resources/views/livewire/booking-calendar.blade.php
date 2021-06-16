c9ff0d8b58681a0aa9e489cb4b8913dc412389e0
Rust <rust.91@icloud.com>
2021-06-16 14:26:28 +0200

    Navigating weeks

---
+++ resources/views/livewire/booking-calendar.blade.php
@@ -0,0 +1,45 @@
+<div class="bg-white rounded-md">
+    <div class="flex items-center justify-center relative">
+        @if($this->weekIsGreaterThanCurrent)
+            <button type="button" class="p-4 absolute left-0 top-0" wire:click="decrementCalendarWeek">
+                <svg class="w-6 h-6 text-gray-400 hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
+            </button>
+        @endif
+
+        <div class="text-lg font-semibold p-4">
+            {{ $this->calendarStartDate->format('F Y') }}
+        </div>
+
+        <button type="button" class="p-4 absolute right-0 top-0" wire:click="incrementCalendarWeek">
+            <svg class="w-6 h-6 text-gray-400 hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
+        </button>
+    </div>
+
+    <div class="flex justify-between items-center px-3 border-b border-gray-300 pb-2">
+        @foreach($this->calendarWeekInterval as $day)
+            <button type="button" class="text-center group focus:outline-none">
+                <div class="text-md leading-none mb-2 text-gray-700">
+                    {{ $day->format('D') }}
+                </div>
+
+                <div class="text-lg leading-none p-1 text-black group-hover:text-white bg-gray-300 font-semibold rounded-full w-9 h-9 group-hover:bg-red-600 flex items-center justify-center">
+                    {{ $day->format('d') }}
+                </div>
+            </button>
+        @endforeach
+    </div>
+
+    <div class="max-h-52 overflow-y-scroll">
+        <input type="radio" name="time" id="" value="" class="sr-only">
+
+        <label for="" class="w-full text-left focus:outline-none px-4 py-2 flex items-center border-b border-gray-200">
+            <svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+            
+            13:00
+        </label>
+
+        <div class="text-center text-red-600 px-4 py-2">
+            Nothing Available
+        </div>
+    </div>
+</div>
--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -27,48 +27,6 @@
             <label for="employee" class="inline-block text-black font-semibold mb-2">Appointment Time</label>
         </div>
 
-        <!-- Bloc 3 - Calendar -->
-        <div class="bg-white rounded-md">
-            <div class="flex items-center justify-center relative">
-                <!-- Back -->
-                <button type="button" class="p-4 absolute left-0 top-0">
-                    <svg class="w-6 h-6 text-gray-400 hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
-                </button>
-
-                <div class="text-lg font-semibold p-4">
-                    January 2021
-                </div>
-
-                <!-- Forwards -->
-                <button type="button" class="p-4 absolute right-0 top-0">
-                    <svg class="w-6 h-6 text-gray-400 hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
-                </button>
-            </div>
-
-            <div class="flex justify-between items-center px-3 border-b border-gray-300 pb-2">
-                <button type="button" class="text-center group focus:outline-none">
-                    <div class="text-md leading-none mb-2 text-gray-700">
-                        Mon
-                    </div>
-
-                    <div class="text-lg leading-none p-1 text-black group-hover:text-white bg-gray-300 font-semibold rounded-full w-9 h-9 group-hover:bg-red-600 flex items-center justify-center">
-                        7
-                    </div>
-                </button>
-            </div>
-
-            <div class="max-h-52 overflow-y-scroll">
-                <input type="radio" name="time" id="" value="" class="sr-only">
-
-                <label for="" class="w-full text-left focus:outline-none px-4 py-2 flex items-center border-b border-gray-200">
-                    <svg class="w-6 h-6 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
-                    13:00
-                </label>
-
-                <div class="text-center text-red-600 px-4 py-2">
-                    Nothing Available
-                </div>
-            </div>
-        </div>
+        <livewire:booking-calendar />
     </form>
 </div>
