fc842044c96a0d9fc352cbe4d0ae0e0b39a8961d
Rust <rust.91@icloud.com>
2021-06-16 21:26:06 +0200

    Booking confirmation

--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -1,8 +1,5 @@
 <div class="bg-gray-100 max-w-lg mx-auto m-6 p-5 rounded-lg">
     <form>
-
-        {{ var_dump($state) }}
-
         <!-- Bloc 1 - Service -->
         <div class="mb-6">
             <label for="service" class="inline-block text-black font-semibold mb-2">Service</label>
@@ -15,7 +12,7 @@
         </div>
 
         <!-- Bloc 2 - Employee -->
-        <div class="mb-6 {{!$employees->count() ? 'opacity-30' : ''}}">
+        <div class="mb-6 {{ !$employees->count() ? 'opacity-30' : '' }}">
             <label for="employee" class="inline-block text-black font-semibold mb-2">Employee</label>
             <select name="employee" id="employee" class="bg-white h-10 w-full border-none rounded-lg" wire:model="state.employee"
                 {{ !$employees->count() ? 'disabled="disabled"' : ''}}>
@@ -26,10 +23,24 @@
             </select>
         </div>
 
-        <div class="mb-6 {{!$this->selectedService || !$this->selectedEmployee ? 'opacity-30' : ''}}">
+        <div class="mb-6 {{ !$this->selectedService || !$this->selectedEmployee ? 'opacity-30' : '' }}">
             <label for="employee" class="inline-block text-black font-semibold mb-2">Appointment Time</label>
 
             <livewire:booking-calendar :service="$this->selectedService" :employee="$this->selectedEmployee" :key="optional($this->selectedEmployee)->id" />
         </div>
+
+        @if ($this->hasDetailsToBook)
+            <div class="mb-6">
+                <div class="text-gray-800 font-bold mb-2">
+                    🎉 Ready to book !
+                </div>
+
+                <div class="border-t border-b border-gray-200 py-2">
+                    Overview: <br><br> {{ $this->selectedService->name }} - {{ $this->selectedService->duration }} minutes with {{ $this->selectedEmployee->name }}
+                    <br> 📅  {{ $this->timeObject->format('D jS M Y') }}
+                    <br> 🕰  {{ $this->timeObject->format('H:i') }}
+                </div>
+            </div>
+        @endif
     </form>
 </div>
